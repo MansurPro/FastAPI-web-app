782fd9a4a2fc8d4f46f32f379efd96a14ae71961
Mansurbek Satarov <mansurbek1203@gmail.com>
2023-07-14 21:16:44 -0500

    Models adding

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from tortoise import Model, fields
 from pydantic import BaseModel
 from datetime import datetime
+from tortoise.contrib.pydantic import pydantic_model_creator
 
 class User(Model):
     id = fields.IntField(pk = True, index = True)
@@ -21,3 +22,16 @@ class Business(Model):
     
 class Product(Model):
     id = fields.IntField(pk = True, index = True)
+    name = fields.CharField(max_length=100, null = False, index = True)
+    category = fields.CharField(max_length=30, index = True)
+    original_price = fields.DecimalField(max_digits=12, decimal_places=2)
+    new_price = fields.DecimalField(max_digits=12, decimal_places=2)
+    percentage_discount = fields.IntField()
+    offer_expiration_date = fields.DateField(default=datetime.utcnow)
+    product_image = fields.CharField(max_length=200, null=False, default="productDefault.jpg")
+    business = fields.ForeignKeyField("models.Business", related_name="products")
+    
+    
+user_pydantic = pydantic_model_creator(User, name="User", exclude=("is_verified", ))
+user_pydanticIn = pydantic_model_creator(User, name="UserIn", exclude_readonly=True)
+user_pydanticOut = pydantic_model_creator(User, name="UserOut", exclude=("password", ))
